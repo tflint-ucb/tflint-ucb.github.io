d892d517ece966fdad8e12513e4bd54771592dc6
tflint-ucb <tanyaflint@berkeley.edu>
2024-02-03 23:33:21 -0500

    Updates

---
+++ component---src-pages-404-tsx-b0731d19411443b81ec1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[296],{8472:function(e,l,n){n.r(l);var t=n(1504),u=n(4260);l.default=e=>t.createElement(u.c,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-b0731d19411443b81ec1.js.map
